bc1a6b8948e2a6e7d79b4986f0c12b448a85617f
Michal Hradis <michal.hradis@gmail.com>
2025-05-12 22:02:06 +0200

    Add argument parsing and filtering options for document queries

--- weaviate_benchmark/query_documents.py
+++ weaviate_benchmark/query_documents.py
@@ -6,8 +6,26 @@ import torch
 from weaviate import WeaviateClient
 from weaviate.connect import ConnectionParams
 from time import time
+import argparse
+from datetime import datetime
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Query Weaviate for text chunks. Can specify a filter.")
+    parser.add_argument("--min-year", type=int, help="Minimum year")
+    parser.add_argument("--max-year", type=int, help="Maximum year")
+    parser.add_argument("--min-date", type=datetime, help="Minimum date")
+    parser.add_argument("--max-date", type=datetime, help="Maximum date")
+    parser.add_argument("--genre", type=str, help="Genre to filter by")
+    parser.add_argument("--author", type=str, help="Author to filter by")
+    parser.add_argument("--title-words", nargs="+", type=str, help="Words to filter by in the title")
+    parser.add_argument("--url", type=str)
+    parser.add_argument("--limit", type=int, default=80, help="Number of results to return")
+    return parser.parse_args()
+
 
 def main():
+    args = parse_args()
     client = WeaviateClient(
         connection_params=ConnectionParams.from_params(
             http_host="localhost", http_port=8080, http_secure=False,
@@ -43,6 +61,37 @@ def main():
         outputs  = model(**inputs)
         q_vector = outputs.last_hidden_state[:,0].cpu().detach().tolist()[0]
 
+        filters = []
+        if args.min_year:
+            filters.append({
+                "path": ["document", "yearIssued"],
+                "operator": "GreaterThan",
+                "valueInt": args.min_year
+            })
+        if args.max_year:
+            filters.append({
+                "path": ["document", "yearIssued"],
+                "operator": "LessThan",
+                "valueInt": args.max_year
+            })
+        if args.genre:
+            filters.append({
+                "path": ["document", "genre"],
+                "operator": "Equal",
+                "valueText": args.genre
+            })
+        if args.author:
+            filters.append({
+                "path": ["document", "author"],
+                "operator": "Equal",
+                "valueText": args.author
+            })
+        if args.title_words:
+            filters.append({
+                "path": ["document", "title"],
+                "operator": "Contains",
+                "valueText": " ".join(args.title_words)
+            })
 
         # 5) Run the vector search
         t1 = time()
